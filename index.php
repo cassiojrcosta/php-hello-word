4475bda8465a9d771fe02f00ce2889b6836fffae
adelmohsn <43905118+adelmohsn@users.noreply.github.com>
2022-11-05 18:49:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php
 
-echo 'Hello World';
+
+<?php
+use Telegram\Bot\Api;
+$telegram =new Api('789483519:AAE8c1b3qbKnFoEGTsnDt6PskxhUbN1jHhY');
+$var =$telegram->getme();
+foreach ($var as $key => $value) {
+$telegram->sendmessage($key.$value);
+
+}
+
+
+
 
 ?>
